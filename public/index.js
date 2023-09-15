8f35a91c7ce9410f33eff0e68336447a41be2b71
hamzalakhwera <hamzakabeer56@gmail.com>
2023-09-15 18:23:33 +0500

    Color update

--- public/index.js
+++ public/index.js
@@ -8,7 +8,6 @@ function init() {
   // const VP = document.getElementById('videoPlayer')
   // const VPToggle = document.getElementById('toggleButton')
 
-  // VPToggle.addEventListener('click', function() {
   //   if (VP.paused) VP.play()
   //   else VP.pause()
   // })
